7cccce748c96536b1febed6d01cc14c68da1ca78
jchaasis <jchaasis@gmail.com>
2018-02-13 13:41:04 -0500

    API call is returning the appropriate data.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Character from './Components/character.js'
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      heroes: '',
+    };
+  }
+
+
+
+  componentWillMount(){
+    var crypto = require('crypto');
+    let ts =  new Date().getTime();
+    let pub_key = 'f31ade9cd87b1035aadb2c59197a80f1';
+    let priv_key = 'd0a850f093335bd68134ea33a7d2b9e15b4307a8'
+    let hash = crypto.createHash('md5').update(ts + priv_key + pub_key).digest('hex');
+    let url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${pub_key}&hash=${hash}`
+    fetch(url)
+       .then(resp => resp.json())
+       .then(response =>
+         console.log(response)
+         // this.setState({
+         //   heroes: response,
+         // })
+       );
+
+  }
   render() {
+
+    console.log(this.heroes)
     return (
       <div className="App">
         <header className="App-header">
-
-
         </header>
-  
+
+        <div>
+          <Character/>
+        </div>
+
+
       </div>
     );
   }
---
+++ src/Components/character.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Character extends Component {
+
+render(){
+
+  return(
+    <div>
+      <p> This is a character </p>
+    </div>
+  )
+}
+
+}
+
+export default Character;
