2da93d9b24f64b8762e32c35c21cecef8ef60882
jchaasis <jchaasis@gmail.com>
2018-04-18 17:02:26 -0400

    Article description appears when the user hovers over the article li.

--- src/Components/NewsStory.js
+++ src/Components/NewsStory.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react';
 
 
 class NewsStory extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = ({
+      hovered: false,
+    })
+  }
+
+  handleEnter(ev){
+    this.setState({
+      hovered: true,
+    })
+  }
+
+  handleExit(ev){
+    this.setState({
+      hovered: false,
+    })
+  }
 
   render(){
 
-    let description = <p> {this.props.story.description} </p>;
+    let description = this.state.hovered === true ? <p className="articleDesc"> {this.props.story.description} </p> : null;
+
 
     let styles = {
       'backgroundImage': `url(${this.props.story.urlToImage})`,
     }
 
     return(
-      <li style={styles} className="article">
+      <li style={styles} className="article" onMouseEnter={()=>this.handleEnter()} onMouseLeave={()=>this.handleExit()}>
         <a href={this.props.story.url} className="articleLink">
         <h4 className="articleTitle"> {this.props.story.title} </h4>
+        {description}
         </a>
       </li>
     );
